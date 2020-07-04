bde0dc2ef2abb9abeeadc5da95520412bb764b06
Bhavya Singhal <bhavyasinghal0601@gmail.com>
2020-07-05 01:15:19 +0530

    Updates

---
+++ precache-manifest.1c0db47a2588bba04546e4ac7c2cffd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce3c027837f9f1f8c53f88e76b7c1c6",
+    "url": "/bhavyasinghal06/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "51a46e882ca9a5c45f62",
+    "url": "/bhavyasinghal06/tic-tac-toe/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df9807fdd2424d9bdc9c",
+    "url": "/bhavyasinghal06/tic-tac-toe/static/css/main.3b6ac00f.chunk.css"
+  },
+  {
+    "revision": "51a46e882ca9a5c45f62",
+    "url": "/bhavyasinghal06/tic-tac-toe/static/js/2.88fe0c6a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/bhavyasinghal06/tic-tac-toe/static/js/2.88fe0c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9807fdd2424d9bdc9c",
+    "url": "/bhavyasinghal06/tic-tac-toe/static/js/main.4e76215f.chunk.js"
+  },
+  {
+    "revision": "99fb3f5de0122b17cf74",
+    "url": "/bhavyasinghal06/tic-tac-toe/static/js/runtime-main.9091ffe2.js"
+  }
+]);
